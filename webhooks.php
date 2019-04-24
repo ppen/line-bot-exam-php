aee27e2eb006bfc23c4e0e55e133e2a3ef7ed855
Phaiboon Pensuwan <ppen5@yahoo.com>
2019-04-24 07:51:58 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'WGU7TOicQwWWXssfpFie6QF3yyUGbv/DEXTmMMKMNH1mZSLslhN0YZcHXYKvDr9+SaRTOUwCp2RHBHbRbaZJ/J5LdVOsteLNGfptuE4vd+oLMQLRNbsa9e3QNXMiBqz1sqeSG8c5UzDCMS2RVHI21gdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
